c1e181463ce00b8056b6c909482c5d3095ba7a11
Jonathan East <jonathan_east@ymail.com>
2020-12-09 17:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 function saturdayFun(string = 'roller-skate') {
   return `This Saturday, I want to ${string}!`
 }
+
+let mondayWork = function(string = 'go to the office') {
+  return `I will ${string}.`
+})
